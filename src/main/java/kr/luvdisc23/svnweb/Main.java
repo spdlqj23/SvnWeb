d40b9d4cc9aed6f8aa27b707d80dd2378fe58667
kimjiho <spdlqj23@naver.com>
2021-05-21 07:41:25 +0900

    페이징 처리 수정

--- src/main/java/kr/luvdisc23/svnweb/Main.java
+++ src/main/java/kr/luvdisc23/svnweb/Main.java
@@ -1,23 +1,7 @@
 package kr.luvdisc23.svnweb;
 
 import io.javalin.Javalin;
-import io.javalin.http.Context;
 import kr.luvdisc23.svnweb.router.SvnRouter;
-import kr.luvdisc23.svnweb.vo.SvnVo;
-import kr.luvdisc23.svnweb.vo.SvnVoPage;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.math.NumberUtils;
-import org.apache.commons.lang3.time.DateFormatUtils;
-import org.jetbrains.annotations.NotNull;
-import org.joda.time.DateTime;
-import org.tmatesoft.svn.core.SVNLogEntry;
-import org.tmatesoft.svn.core.SVNURL;
-import org.tmatesoft.svn.core.auth.BasicAuthenticationManager;
-import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
